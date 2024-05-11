7211b1260f5b29198546e76a923c65d557f7e5d8
Sachin Mahato <mahatosachin5@gmail.com>
2024-05-11 13:32:15 +0530

    updates

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -6,6 +6,30 @@
 
 function isAnagram(str1, str2) {
 
+ // checking the length of both the strings are equal
+  if (str1.length !== str2.length) {
+    return false;
+  }
+ //  first changing string to Lower Case and then truning it into an Array  
+  let newArr1 = Array.from(str1.toLowerCase());
+  let newArr2 = Array.from(str2.toLowerCase());
+
+ //1st  array1.slice() creates a shallow copy of array1 with the same elements [1, 2, 3]. Since no arguments are provided, it copies the entire array. 
+
+ // 2nd n your code, array1.slice().sort() sorts the shallow copy of array1 created by slice(). Since no comparison function is provided, it sorts the elements lexicographically(accending) as strings by default.
+  const sortedArr1 = newArr1.slice().sort();
+  const sortedArr2 = newArr2.slice().sort();
+
+//   It uses the every() method to iterate over elements of sortedArr1.
+// For each element, it compares the element with the corresponding element at the same index in sortedArr2.
+// If all elements at corresponding indices are equal, it returns true; otherwise, it returns false.
+  return sortedArr1.every((element, index) => element === sortedArr2[index]);
+
+
 }
 
+
 module.exports = isAnagram;
+
+
+
